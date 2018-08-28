f55e917a02b43cb6846043fdb0fd3a959d9c0737
Ahmer Butt <ahmer.butt@vivacitylabs.com>
2018-08-28 14:15:18 +0100

    lots of api routes for demonstrating associations

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import Sequelize from 'sequelize';
 import { Request, Response } from 'express';
 import { createModels } from './models';
 import { CommentInstance } from 'models/Comment';
 import { PostInstance } from 'models/Post';
 import { UserInstance } from 'models/User';
+
+const Op = Sequelize.Op;
 
 const app: express.Application = express();
 
@@ -31,6 +34,94 @@ app.get('/posts', (req: Request, res: Response) => {
   db.Post.findAll()
     .then((posts: PostInstance[]) => res.status(200).json({ posts }))
     .catch(err => res.status(500).json({ err: ['oops', err] }));
+});
+
+// GET all users that upvoted a comment
+app.get('/comments/:id/upvoters', (req: Request, res: Response) => {
+  db.Comment.findById(req.params.id)
+    .then(comment => comment.getUpvoters())
+    .then(upvoters =>
+      res.status(200).json({
+        user_ids: upvoters.map(user => user.id)
+      })
+    )
+    .catch(err => res.status(500).json({ err: ['oops', err] }));
+});
+
+// GET all posts a user has authored
+app.get('/users/:id/posts', (req: Request, res: Response) => {
+  db.User.findById(req.params.id)
+    .then(user => user.getPosts())
+    .then(posts => 
+      res.status(200).json({ posts })
+    )
+    .catch(err => res.status(500).json({ err: ['oops', err] }))
+});
+
+// POST create new dummy user with posts
+app.post('/seed/user', (req: Request, res: Response) => {
+  db.User.create({
+    name: 'John Doe',
+    posts: [
+      {
+        name: 'post1',
+        title: 'Croissants are tasty',
+        text: 'I recently ate a croissant from France. It was nice.',
+        category: 'croissants'
+      },
+      {
+        name: 'post2',
+        title: 'my fav techno music',
+        text: 'I love the song TECHNO by TechnoGang.',
+        category: 'techno'
+      }
+    ]
+  })
+    .then(user => res.status(201).json({ user }))
+    .catch(err => res.status(500).json({ err: ['oops', err] }))
+});
+
+// POST create new dummy upvotes for comment
+// also, use async/await syntax here, just to demonstrate you can do it in typescript
+app.post('/seed/user/:user_id/upvotes', async (req: Request, res: Response) => {
+  try {
+    const user = await db.User.findById(req.params.user_id);
+    const comment = await user.createComment({
+      text: 'this comment is cool.'
+    });
+    await user.addUpvotedComment(comment.id);
+    res.status(201).json({ comment });
+  } catch (err) {
+    res.status(500).json({ err: ['oops', err] });
+  }
+});
+
+// POST add upvoter
+app.post('/comment/:comment_id/upvoter/:upvoter_id', (req: Request, res: Response) => {
+  db.Comment.findById(req.params.comment_id)
+    .then(comment => comment.addUpvoter(req.params.upvoter_id))
+    .catch(err => res.status(500).json({ err: ['oops', err] }))
+});
+
+// GET all upvoted comments for specified users (using eager loading)
+// userIds go in query params, e.g. /upvoted_comments?userIds=[1,2,3]
+app.get('/upvoted_comments', (req: Request, res: Response) =>  {
+  console.log(req.query.userIds);
+  const userIds = JSON.parse(req.query.userIds);
+  db.User.findAll({
+    where: { id: { [Op.in]: userIds } },
+    include: [{ model: db.Comment, as: 'upvotedComments' }]
+  })
+    .then(users => {
+      const flatten = (flattened: CommentInstance[], next: CommentInstance[]) => ([
+        ...flattened,
+        ...next
+      ]);
+      const comments = users.map(user => user.upvotedComments)
+        .reduce(flatten, []);
+      res.status(200).json({ upvotedComments: comments });
+    })
+    .catch(err => res.status(500).json({ err: ['oops', err] }))
 });
 
 app.listen(3000, () => {
